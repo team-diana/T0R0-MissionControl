dfea3eb4fba878d234693510166e9d984130e466
DunkelAmber <frama95@gmail.com>
2018-05-29 22:14:19 +0200

    add color header

---
+++ src/library/colors.h
@@ -0,0 +1,24 @@
+#ifndef COLORS_H
+#define COLORS_H
+#include <QColor>
+
+QColor color_window_bk (14, 14, 14);
+QColor color_widget_bk (10, 10, 10);
+
+QColor serious_red(180, 19, 26)
+QColor serious_green (82, 190, 91);
+
+QColor bright_green (45, 229, 171);
+QColor bright_yellow (255, 204, 0);
+QColor bright_red (255, 59, 48);
+QColor bright_blue (0, 149, 255);
+
+QColor pale_lightblue (90, 200, 250);
+QColor pale_red (252, 45, 85);
+QColor pale_violet (135, 135, 255);
+
+QColor indicator_green (53, 203, 75);
+QColor indicator_yellow (253, 189, 65);
+QColor indicator_red (252, 98, 93);
+
+#endif // COLORS_H
