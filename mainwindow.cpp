89945d02a42427b8ef7d025de43dcb379728a531
DunkelAmber <frama95@gmail.com>
2018-07-06 19:25:48 +0200

    Added Qt Creator support
    From now on, this project will be compiled within Qt Creator

---
+++ mainwindow.cpp
@@ -0,0 +1,72 @@
+#include "mainwindow.h"
+#include <QKeyEvent>
+#include <QCoreApplication>
+#include <QDebug>
+
+
+#define ROVIMG_WIDTH 230
+#define ROVIMG_HEIGHT 374
+
+// Constructor
+MainWindow::MainWindow(QWidget *parent) : QWidget(parent)
+{
+  //this->setGeometry(0, 0, screen.width(), screen.height());   // -50
+  this->setGeometry(0, 0, 1920, 1080);
+
+  // set black background
+  QPalette pal = palette();
+
+    // Set backgroud color
+    pal.setColor(QPalette::Background, QColor(14,14,14,255));
+  this->setAutoFillBackground(true);
+  this->setPalette(pal);
+
+    // Show Rover Image at the center of the Window
+  QLabel *pixlabel = new QLabel(this);
+  QPixmap pixrover(":/Images/rover_up-230x374.png");
+    pixlabel->setPixmap(pixrover.scaled(ROVIMG_WIDTH, ROVIMG_HEIGHT, Qt::KeepAspectRatio));
+    pixlabel->setGeometry( (this->width() - ROVIMG_WIDTH) /2, (this->height() - ROVIMG_HEIGHT) /2, ROVIMG_WIDTH, ROVIMG_HEIGHT );
+
+    // Show Team D.I.A.N.A. logo at bottom-right
+    QLabel *pixlogo = new QLabel(this);
+  QPixmap pixmaplogo(":/Images/diana-logo-193x90.png");
+    pixlogo->setPixmap(pixmaplogo.scaled(193, 90, Qt::KeepAspectRatio));
+    pixlogo->setGeometry( 50, this->height() - 120, 193, 90 );
+
+  connected = false;
+
+  // Custom elements
+  start_button = new StartButton(this);
+  start_button->setGeometry(this->width() / 2 - 100, this->height() - 100, 200, 100);
+
+    batterydisplay = new BatteryDisplay(this);
+    batterydisplay->setGeometry(BAT_DISPLAY_POSX, BAT_DISPLAY_POSY, BAT_DISPLAY_WIDTH + 2, BAT_DISPLAY_HEIGHT + 2);
+
+    statusdisplay = new StatusDisplay(this);
+    statusdisplay->setGeometry(STATUS_DISPLAY_POSX, (this->height() - STATUS_DISPLAY_HEIGHT) / 2, STATUS_DISPLAY_WIDTH + 2, STATUS_DISPLAY_HEIGHT + 2);
+
+    autosysdisplay = new AutoSysDisplay(this);
+    autosysdisplay->setGeometry(AUTOSYS_DISPLAY_POSX, AUTOSYS_DISPLAY_POSY, AUTOSYS_DISPLAY_WIDTH + 2, AUTOSYS_DISPLAY_HEIGHT + 2);
+
+    radiodisplay = new RadioDisplay(this);
+    radiodisplay->setGeometry(RADIO_DISPLAY_POSX, RADIO_DISPLAY_POSY, RADIO_DISPLAY_WIDTH + 2, RADIO_DISPLAY_HEIGHT + 2);
+
+    allsysdisplay = new AllSysDisplay(this);
+    allsysdisplay->setGeometry( ((this->width() / 2) - (ALLSYS_DISPLAY_WIDTH / 2)), ALLSYS_DISPLAY_POSY, ALLSYS_DISPLAY_WIDTH + 2, ALLSYS_DISPLAY_HEIGHT + 2);
+}
+
+MainWindow::~MainWindow(){
+}
+
+void MainWindow::keyPressEvent (QKeyEvent *k) {
+    switch ( tolower(char(k->key())) ) {
+            case 'r':                               // reload
+                qDebug("Pressed R");//void keyPressEvent(QKeyEvent *k) Q_DECL_OVERRIDE;
+                update();
+                break;
+            case 'q':                               // quit//void keyPressEvent(QKeyEvent *k) Q_DECL_OVERRIDE;
+              qDebug("Pressed Q: Quitting application");
+              QCoreApplication::quit();
+                break;
+        }
+}
---
+++ mainwindow.h
@@ -0,0 +1,51 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "rovershow.h"
+#include "startbutton.h"
+#include "batteryindicator.h"
+#include "batterypanel.h"
+#include "batterydisplay.h"
+#include "statusdisplay.h"
+#include "imagebutton.h"
+#include "autosysdisplay.h"
+#include "radiodisplay.h"
+#include "allsystemsdisplay.h"
+
+#include <QLabel>
+
+#include <QString>
+
+#define XPOS_BATTGROUP 0
+#define YPOS_BATTGROUP 0
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+public slots:
+
+protected:
+    void keyPressEvent(QKeyEvent *e);
+
+private:
+    bool connected;
+
+    // Custom elements
+    AllSysDisplay *allsysdisplay;
+    StartButton *start_button;
+    RoverShow *rover;
+    BatteryPanel *batterypanel1;
+    BatteryDisplay *batterydisplay;
+    StatusDisplay *statusdisplay;
+    QLabel *pixlabel;
+    AutoSysDisplay *autosysdisplay;
+    RadioDisplay *radiodisplay;
+
+};
+
+#endif // MAINWINDOW_H
