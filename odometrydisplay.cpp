b12edae986aceba55673b38d8ba4292db0bf89d9
DunkelAmber <frama95@gmail.com>
2018-09-14 02:49:36 +0200

    fix: odometry not dont show properly

--- odometrydisplay.cpp
+++ odometrydisplay.cpp
@@ -40,43 +40,43 @@ void OdomDisplay::paintEvent(QPaintEvent *)
     // qDebug() << "Odometry: X " << x;
     painter.setPen(QColor(222, 61, 25));
     painter.drawText(QRect(10, 35, 135, 20), Qt::AlignLeft, "X");
-    // ValueString.sprintf("%5.3f", x);
-    painter.drawText(QRect(110, 35, 220, 65), Qt::AlignLeft, ValueString);
+    ValueString.sprintf("%5.3f", x);
+    painter.drawText(QRect(65, 35, 220, 65), Qt::AlignLeft, ValueString);
 
 	ValueString.begin();
     // qDebug() << "Odometry: Y " << y;
     painter.setPen(QColor(222, 61, 25));
     painter.drawText(QRect(10, 55, 135, 20), Qt::AlignLeft, "Y");
-    // ValueString.sprintf("%5.3f", y);
-    painter.drawText(QRect(110, 35, 220, 65), Qt::AlignLeft, ValueString);
+    ValueString.sprintf("%5.3f", y);
+    painter.drawText(QRect(65, 55, 220, 65), Qt::AlignLeft, ValueString);
 
 	ValueString.begin();
     // qDebug() << "Odometry: Z " << z;
     painter.setPen(QColor(222, 61, 25));
     painter.drawText(QRect(10, 75, 135, 20), Qt::AlignLeft, "Z");
-    // ValueString.sprintf("%5.3f", z);
-    painter.drawText(QRect(110, 35, 220, 65), Qt::AlignLeft, ValueString);
+    ValueString.sprintf("%5.3f", z);
+    painter.drawText(QRect(65, 75, 220, 65), Qt::AlignLeft, ValueString);
 
 	ValueString.begin();
     // qDebug() << "Odometry: Pitch " << pitch;
     painter.setPen(QColor(222, 61, 25));
-    painter.drawText(QRect(110, 35, 135, 20), Qt::AlignLeft, "Pitch");
-    // ValueString.sprintf("%5.3f", pitch);
-    painter.drawText(QRect(210, 35, 220, 65), Qt::AlignLeft, ValueString);
+    painter.drawText(QRect(175, 35, 135, 20), Qt::AlignLeft, "Pitch");
+    ValueString.sprintf("%5.3f", pitch);
+    painter.drawText(QRect(250, 35, 220, 65), Qt::AlignLeft, ValueString);
 
 	ValueString.begin();
     // qDebug() << "Odometry: Roll " << roll;
     painter.setPen(QColor(222, 61, 25));
-    painter.drawText(QRect(110, 55, 135, 20), Qt::AlignLeft, "Roll");
-    // ValueString.sprintf("%5.3f", roll);
-    painter.drawText(QRect(210, 35, 220, 65), Qt::AlignLeft, ValueString);
+    painter.drawText(QRect(175, 55, 135, 20), Qt::AlignLeft, "Roll");
+    ValueString.sprintf("%5.3f", roll);
+    painter.drawText(QRect(250, 55, 220, 65), Qt::AlignLeft, ValueString);
 
 	ValueString.begin();
     // qDebug() << "Odometry: Yaw " << yaw;
     painter.setPen(QColor(222, 61, 25));
-    painter.drawText(QRect(110, 75, 135, 20), Qt::AlignLeft, "Yaw");
-    // ValueString.sprintf("%5.3f", yaw);
-    painter.drawText(QRect(210, 35, 220, 65), Qt::AlignLeft, ValueString);
+    painter.drawText(QRect(175, 75, 135, 20), Qt::AlignLeft, "Yaw");
+    ValueString.sprintf("%5.3f", yaw);
+    painter.drawText(QRect(250, 75, 220, 65), Qt::AlignLeft, ValueString);
 }
 
 void OdomDisplay::odometryUpdate(QString type, float value)
